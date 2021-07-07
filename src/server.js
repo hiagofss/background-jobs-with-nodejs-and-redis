73e4d26a98acf7ec419d22ebf3968460cf25bd00
Hiago Souza <hiagofss98@gmail.com>
2021-07-07 20:19:34 -0300

    Base project

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import express from 'express';
+
+const port = process.env.PORT || 3333;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+	console.log(`Server running ${port}`);
+});
