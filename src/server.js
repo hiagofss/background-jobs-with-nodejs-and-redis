93dacab4b8421c6f13b3111c66822bc9b1f29024
Hiago Souza <hiagofss98@gmail.com>
2021-07-12 21:18:50 -0300

    Implements BullBoard

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 import 'dotenv/config';
 import express from 'express';
+import BullBoard from 'bull-board';
 
 import UserController from './app/controller/UserController';
+import Queue from './app/lib/Queue';
 
 const port = process.env.PORT || 3333;
 
 const app = express();
+BullBoard.setQueues(Queue.queues.map((queue) => queue.bull));
 
 app.use(express.json());
 
 app.post('/users', UserController.store);
+
+app.use('/admin/queues', BullBoard.UI);
 
 app.listen(port, () => {
 	console.log(`Server running ${port}`);
